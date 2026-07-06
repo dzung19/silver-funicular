94693c2aff58b7bf95538fa22564db6caf2f2778
Dung <phungdung14@gmail.com>
2026-07-06 14:46:37 +0700

    update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,11 +6,7 @@ plugins {
 
 android {
     namespace = "com.dzungphung.smarthome"
-    compileSdk {
-        version = release(36) {
-            minorApiLevel = 1
-        }
-    }
+    compileSdk =37
 
     defaultConfig {
         applicationId = "com.dzungphung.smarthome"
